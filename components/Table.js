57de564f5ff629ed3d45d3c63e13752ab0a9c6ff
Rachael <rachnbob@hotmail.co.uk>
2017-11-14 13:41:04 +0000

    Added new leaderboard component, change the import of prop types to import React, { Component } from 'react';import PropTypes from 'prop-types'; in index.js in node modules

--- components/Table.js
+++ components/Table.js
@@ -8,68 +8,34 @@ import {
   ListView,
 } from 'react-native';
 
-import {
-  Table,
-  TableWrapper,
-  Row,
-  Rows,
-  Col,
-  Cols,
-  Cell
-} from 'react-native-table-component';
-
 import Db from '../data/Db';
 
+import Leaderboard from 'react-native-leaderboard';
 
 const leaderboard = Db.getLeaderboard();
 const tableHead = ['User ID', 'Username', 'Score'];
-const tableData = [
-
-  [leaderboard[0].userID, leaderboard[0].username, leaderboard[0].score],
-  [leaderboard[1].userID, leaderboard[1].username, leaderboard[1].score],
-  [leaderboard[2].userID, leaderboard[2].username, leaderboard[2].score],
-  [leaderboard[3].userID, leaderboard[3].username, leaderboard[3].score],
-  [leaderboard[4].userID, leaderboard[4].username, leaderboard[3].score],
-
-];
 
 export default class TableView extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+        data: [
+          {userName:leaderboard[0].username, highScore:leaderboard[0].score},
+          {userName:leaderboard[1].username, highScore:leaderboard[1].score},
+          {userName:leaderboard[2].username, highScore:leaderboard[2].score},
+          {userName:leaderboard[3].username, highScore:leaderboard[3].score},
+          {userName:leaderboard[4].username, highScore:leaderboard[3].score}
+        ]
+    }
+  }
+
   render() {
     return (
-      <View>
-        <Text style={styles.title}> XPlore Lancaster Leaderboard </Text>
-
-        <Table>
-          <Row data = {tableHead} style={styles.head} textStyle={styles.text}/>
-          <Rows data = {tableData} style={styles.row} textStyle={styles.text}/>
-        </Table>
-
-      </View>
-    )
+        <Leaderboard
+          data={this.state.data}
+          sortBy='highScore'
+          labelBy='userName'/>
+        )
   }
-}
-
-const styles = StyleSheet.create({
-  head: {
-      height: 30,
-      backgroundColor: 'pink',
-      marginTop: 100,
-  },
-
-  text: {
-    marginLeft: 5,
-    textAlign: 'center',
-  },
-
-  row: {
-    height: 35,
-
-   },
-   title: {
-     color: 'black',
-     textAlign: 'center',
-     fontSize: 35,
-     marginTop: 20,
-     fontFamily: 'calibri',
-   }
-});
+};
