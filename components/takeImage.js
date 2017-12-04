3ce821c40a6ce6e40dc75a1671780fd02872ddd4
Tom Donnellon <jmdnnlln@live.com>
2017-12-04 12:43:50 +0000

    Buttons added, limited functionalily on opacity

--- components/takeImage.js
+++ components/takeImage.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, Dimensions, Image} from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity} from 'react-native';
 import { FormLabel, FormInput, Button, Divider } from 'react-native-elements';
 import Camera from 'react-native-camera';
+import NavButtons from './NavButtons';
 
 export default class TakeImage extends Component
 {   
@@ -25,7 +26,7 @@ export default class TakeImage extends Component
 
   setData = (camData) => {
     console.log(camData);
-    this.setState({data: 'file:///storage/emulated/0/DCIM/IMG_20171128_165857.jpg'});
+    this.setState({data: camData.path});
     this.setState({renderCamera: false})
   }
 
@@ -45,8 +46,11 @@ export default class TakeImage extends Component
     let renderCamera = this.state.renderCamera;
 
     let view = null;
+      
       return ( 
-        (renderCamera && <Camera 
+      (renderCamera && 
+        <View>
+          <Camera 
             ref={(cam) => {this.camera = cam;}}
             style={styles.preview}
             aspect={Camera.constants.Aspect.fill}>
@@ -55,10 +59,25 @@ export default class TakeImage extends Component
             [Take Picture!]
             </Text>
 
-          </Camera>) ||
-        (!renderCamera &&
-            <Image style={{flex: 1}} source={{uri: this.state.data}} />)
-      );
+          </Camera>
+          <NavButtons
+            navigation={this.props.navigation}
+            showBack={true}
+            showBurger={false}
+          />
+        </View>
+      ) ||
+      (!renderCamera && 
+        <View> 
+          <Image style={styles.camPreview} source={{uri: this.state.data}} />
+          <NavButtons
+          navigation={this.props.navigation}
+          showBack={true}
+          showBurger={false}
+          /> 
+        </View>
+      )
+    );
   }
 }
 
@@ -70,7 +89,6 @@ preview: {
  alignItems: 'center',
  height: Dimensions.get('window').height,
  width: Dimensions.get('window').width
-
 },
 capture: {
   flex: 0,
@@ -79,5 +97,13 @@ capture: {
   color: '#000',
   padding: 10,
   margin: 40
-}
+},
+camPreview: {
+  flex: 1
+},
+button: {
+    width: 50,
+    height: 50,
+    margin: 10,
+  }
 });
